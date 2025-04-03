31659e0f7e2354c7fc55ae2f0e50dcfe505ed89b
Zbalah Addem <zbalah3@mmi-dev.unilim.fr>
2025-04-03 09:37:41 +0200

    update credentials

--- server/model.php
+++ server/model.php
@@ -14,6 +14,6 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "mora");
-define("DBLOGIN", "root");
-define("DBPWD", "root");
+define("DBNAME", "zbalah3");
+define("DBLOGIN", "zbalah3");
+define("DBPWD", "zbalah3");
