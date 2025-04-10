baf23ec3d288634667466c95cbc3f2ccfdb17c8d
AddemZbalah <addem.zbalah@etu.unilim.fr>
2025-04-10 11:27:03 +0200

    ité 6 fini on commence ité 7

--- app/data/dataProfile.js
+++ app/data/dataProfile.js
@@ -3,9 +3,15 @@ let HOST_URL = "https://mmi.unilim.fr/~zbalah3/SAE2.03-Addem-Zbalah/";
 let DataProfile = {};
 
 DataProfile.request = async function(){
-    let answer = await fetch(HOST_URL + "/server/script.php?todo=getUsersProfiles");  
-    let data = await answer.json();
-    return data;
+    try {
+        // Correction du chemin vers le script PHP
+        let answer = await fetch("../server/script.php?todo=getUsersProfiles");  
+        let data = await answer.json();
+        return data;
+    } catch (error) {
+        console.error("Erreur de requête:", error);
+        return [];
+    }
 }
 
 export {DataProfile};
